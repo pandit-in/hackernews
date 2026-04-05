febf4b4b057548cd459741ec253ee9c72e9ecbf7
Pandit Pawar <iampandit.in@gmail.com>
2026-04-05 16:19:44 +0530

    initial commit

---
+++ packages/api/src/routers/todo.ts
@@ -0,0 +1,34 @@
+import { createDb } from "@hackernews/db";
+import { todo } from "@hackernews/db/schema/todo";
+import { eq } from "drizzle-orm";
+import z from "zod";
+
+import { router, publicProcedure } from "../index";
+
+export const todoRouter = router({
+  getAll: publicProcedure.query(async () => {
+    const db = createDb();
+    return await db.select().from(todo);
+  }),
+
+  create: publicProcedure
+    .input(z.object({ text: z.string().min(1) }))
+    .mutation(async ({ input }) => {
+      const db = createDb();
+      return await db.insert(todo).values({
+        text: input.text,
+      });
+    }),
+
+  toggle: publicProcedure
+    .input(z.object({ id: z.number(), completed: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const db = createDb();
+      return await db.update(todo).set({ completed: input.completed }).where(eq(todo.id, input.id));
+    }),
+
+  delete: publicProcedure.input(z.object({ id: z.number() })).mutation(async ({ input }) => {
+    const db = createDb();
+    return await db.delete(todo).where(eq(todo.id, input.id));
+  }),
+});
